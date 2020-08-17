21e6192b83e7db3fd14ce1c02772872f95aa9d53
Dave Borghuis <dave@daveborghuis.nl>
2020-08-17 21:01:01 +0200

    Changed the test tool, show all beacons

--- testbeacontool.py
+++ testbeacontool.py
@@ -6,20 +6,14 @@ from beacontools import BeaconScanner, ExposureNotificationFrame
 beacon = ''
 
 def callback(bt_addr, rssi, packet, additional_info):
-    global beacon
-    tempbeacon = str(additional_info)[16:48]
-    #print("Temp %s",tempbeacon)
-    #print(str(additional_info)[16:48])
-    if beacon != tempbeacon:
-        now = str(datetime.now())
-        beacon = str(additional_info)[16:48]
-        print(now +" beacon: " +beacon)
+    print(str(datetime.now()) +" beacon: " +str(additional_info)[16:48]+' rsst:'+str(rssi))
     #print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
 
 # scan for all COVID-19 exposure notifications
 scanner = BeaconScanner(callback, 
+    #comment next line if you want to see ALL beacons
     packet_filter=[ExposureNotificationFrame]
 )
 scanner.start()
-time.sleep(12*60*60)
+time.sleep(10*60) #scan for 10 mins
 scanner.stop()
