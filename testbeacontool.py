067713fa1f76970126fa4d4ef5d075ba498fafff
Dave Borghuis <dave@daveborghuis.nl>
2020-08-30 17:49:33 +0200

    Cleanup testtool, added Desktop image

--- testbeacontool.py
+++ testbeacontool.py
@@ -1,19 +1,17 @@
 import time
-import json
-from datetime import datetime
 from beacontools import BeaconScanner, ExposureNotificationFrame
 
-beacon = ''
-
 def callback(bt_addr, rssi, packet, additional_info):
-    print(str(bt_addr)+str(time.time()) +" beacon: " +str(additional_info)[16:48]+' rsst:'+str(rssi))
-    #print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+    print(str(time.strftime("%H:%M:%S", time.localtime(time.time())))+" "+str(bt_addr)+" ("+str(rssi)+") " +str(additional_info)[16:48])
 
 # scan for all COVID-19 exposure notifications
 scanner = BeaconScanner(callback, 
-    #comment next line if you want to see ALL beacons
-    #packet_filter=[ExposureNotificationFrame]
+    packet_filter=[ExposureNotificationFrame]
 )
+
+
+print("Start scanning for 10 secs.")
 scanner.start()
-time.sleep(10*60) #scan for 10 mins
+time.sleep(10) 
 scanner.stop()
+print("Scanning stopped.")
